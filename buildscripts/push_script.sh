d9e7c732b4da92130c2206228ae4991ca4f326c9
Rusk85 <troggs@gmx.net>
2017-05-14 02:40:26 +0200

    fixed path in run script;added function to increment version semantically

--- buildscripts/push_script.sh
+++ buildscripts/push_script.sh
@@ -2,23 +2,59 @@
 # script for staging everything, committing and pushing
 # from there docker builds the image
 
-set -o errexit    # abort script at first error
-set -o pipefail   # return the exit status of the last command in the pipe
-set -o nounset    # treat unset variables and parameters as an error
+set -o errexit	# abort script at first error
+set -o pipefail	# return the exit status of the last command in the pipe
+set -o nounset	# treat unset variables and parameters as an error
+set -x		# Use during debugging
 
-# block for creating proper version number
-# version pattern: major.minor.revision
-# exmaple: 1.5.99
+if [ "$#" -ne 2 ] ; then
+	printf "\nUsage:\n"
+	printf "\$1: {--major|--minor|--patch} # Increments either by +1\n"
+	printf "\$2: commit message # e.g.: \"Added new feature\"\n"
+	exit 1
+fi
+
 VERSION_FILE=$(pwd)/../cfgs/VERSION
 VERSION=$(cat $VERSION_FILE)
-COMMIT_MSG="$VERSION"
-if [ "$#" -eq 1 ]; then
-	COMMIT_MSG="$VERSION: $1"
-fi
+COMMIT_MSG=$2
+
+increment_version()
+{
+	local arg=$1
+	local version_arr=(${VERSION//\./ })
+	local major="--major"
+        local minor="--minor"
+       	local patch="--patch"
+	local ma=${version_arr[0]}
+	local mi=${version_arr[1]}
+	local pa=${version_arr[2]}
+	if [ "$arg" = "$patch" ] ; then
+		let "pa=pa+1"
+	elif [ "$arg" = "$minor" ] ; then
+		let "mi=mi+1"
+		pa=0
+	elif [ "$arg" = "$major" ] ; then
+		let "ma=ma+1"
+		mi=0
+		pa=0
+	else
+		exit 1
+	fi
+	VERSION=$ma.$mi.$pa
+}
+
+update_version_file()
+{
+	echo $VERSION > $VERSION_FILE
+	printf "\nUpdated Version in $VERSION_FILE to $(cat ${VERSION_FILE})\n"
+}
 
 printf "\nAdding, committing and pushing changes to origin...\n"
 
+increment_version $1
+update_version_file
 git add .
-git commit -m "$COMMIT_MSG"
+git commit -am "$COMMIT_MSG"
 git push origin
 
+exit 0
